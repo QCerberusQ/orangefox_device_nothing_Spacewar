73d181fd72ed1d216e508ba52d248035f04d1f07
Furkan Enes <113352008+QCerberusQ@users.noreply.github.com>
2026-01-25 03:21:38 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,18 +1,14 @@
 #
-# Copyright (C) 2021 The Lineage Open Source Project
+# SPDX-License-Identifier: Apache-2.0
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# OrangeFox / TWRP AndroidProducts
+# Nothing Phone (1) - Spacewar
 #
 
 PRODUCT_MAKEFILES := \
-     $(LOCAL_DIR)/twrp_Spacewar.mk
+    $(LOCAL_DIR)/twrp_Spacewar.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_Spacewar-user \
+    twrp_Spacewar-userdebug \
+    twrp_Spacewar-eng
