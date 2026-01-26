83da1b6022aa4a645ac47a8ff7be664bc35748e3
Furkan Enes <113352008+QCerberusQ@users.noreply.github.com>
2026-01-26 15:44:34 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -129,7 +129,8 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libdisplayconfig.qti \
     libion \
     vendor.display.config@1.0 \
-    vendor.display.config@2.0
+    vendor.display.config@2.0 \
+    libdisplayconfig.qti
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
