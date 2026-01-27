afe3c5840b30ef521221047b08908b22ea31beb0
Furkan Enes <113352008+QCerberusQ@users.noreply.github.com>
2026-01-27 22:09:52 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -95,16 +95,7 @@ PRODUCT_PACKAGES += \
 # -----------------------------------------------------------------------------
 # Vendor DLKM Modules (Eski Dosyadan 'goodix_fp' Eklendi)
 # -----------------------------------------------------------------------------
-TW_LOAD_VENDOR_MODULES := \
-    modules.load \
-    adsp_loader_dlkm.ko \
-    msm_drm.ko \
-    q6_notifier_dlkm.ko \
-    q6_pdr_dlkm.ko \
-    sensors_ssc.ko \
-    qti_battery_charger_main.ko \
-    fts_tp.ko \
-    goodix_fp.ko
+TW_LOAD_VENDOR_MODULES := "modules.load adsp_loader_dlkm.ko msm_drm.ko q6_notifier_dlkm.ko q6_pdr_dlkm.ko sensors_ssc.ko qti_battery_charger_main.ko fts_tp.ko goodix_fp.ko"
 
 TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 
