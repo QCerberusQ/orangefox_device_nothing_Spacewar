2c6ae8d8ed1cabb67f8b5a031d342c677d4ab685
Furkan Enes <113352008+QCerberusQ@users.noreply.github.com>
2026-01-28 03:30:28 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.system.keystore2 \
     android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-service-qti \
     qcom_decrypt \
     qcom_decrypt_fbe
 
