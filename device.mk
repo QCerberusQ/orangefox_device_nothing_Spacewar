37413332737fb52e0fcf3d9e0fb2835839f02109
QCerberusQ <113352008+QCerberusQ@users.noreply.github.com>
2026-05-22 00:45:37 +0300

    Update device.mk with FUSE passthrough and fstab
    Add FUSE passthrough property and update fstab path.

--- device.mk
+++ device.mk
@@ -129,3 +129,10 @@ SOONG_CONFIG_ufsbsg_ufsframework := bsg
 # FUSE PASSTHROUGH
 # -----------------------------------------------------------------------------
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.fuse.passthrough.enable=true
+
+# -----------------------------------------------------------------------------
+# First Stage Ramdisk FSTAB
+# -----------------------------------------------------------------------------
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/root/system/etc/recovery.fstab:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/system/etc/fstab.default
+
