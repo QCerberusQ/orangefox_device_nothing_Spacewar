920dd1da2966ea8869ba483ee9a788fb1cd46721
QCerberusQ <113352008+QCerberusQ@users.noreply.github.com>
2026-06-28 15:08:26 +0300

    Remove PRODUCT_TARGET_VNDK_VERSION from device.mk

--- device.mk
+++ device.mk
@@ -31,7 +31,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_SHIPPING_API_LEVEL := 31
-PRODUCT_TARGET_VNDK_VERSION := 31
 
 # -----------------------------------------------------------------------------
 # A/B OTA
